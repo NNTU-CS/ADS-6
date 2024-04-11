f45d59c6e69f2e4c763083b2338846ca7c444dcf
Tsvetov1984 <acvetov68@gmail.com>
2024-04-11 20:09:08 +0300

    ASD-6

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,14 +2,41 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
+#include <string>
+
 template<typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
+ private:
+    int begin, end, count;
+    T *data;
+ public:
+    TPQueue() : begin(0), end(0), count(0), data(new T[size]) {}
+
+    T pop() {
+        if (count == 0) {
+            throw std::string("Empty!!!");
+        } else {
+            count--;
+            return data[begin++ % size];
+        }
+    }
+
+    void push(const T &item) {
+        if (count >= size)
+            throw std::string("Full!!!");
+        int Index = end;
+        while (Index > begin && data[(Index - 1) % size].prior < item.prior) {
+            data[Index % size] = data[(Index - 1) % size];
+        }
+        data[Index % size] = item;
+        end = (end + 1) % size;
+        count++;
+    }
 };
 
 struct SYM {
-  char ch;
-  int prior;
+    char ch;
+    int prior;
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
