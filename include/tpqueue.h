75e4961d7590b742539e3f856d1792cd8f8b4b07
Funibi <139475082+Funibi@users.noreply.github.com>
2024-04-12 15:31:18 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,9 +2,30 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
-template<typename T, int size>
+template <typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
+ private:
+  int end = 0, begin = 0, count;
+  T* arr;
+
+ public:
+  TPQueue() : arr(new T[size]), begin(0), end(0), count(0) {}
+  void push(const T& value) {
+    int i;
+    for (i = end; i >= begin; i--) {
+      if (value.prior > arr[i].prior)
+        arr[i + 1] = arr[i];
+      else
+        break;
+    }
+
+    arr[i + 1] = value;
+    end++;
+  }
+  T& pop() {
+    end--;
+    return arr[begin++ % size];
+  }
 };
 
 struct SYM {
