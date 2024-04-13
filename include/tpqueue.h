9431b0ab0dbe9089552b673faf772d2ee46a4ad3
krizal1 <159939607+krizal1@users.noreply.github.com>
2024-04-13 18:10:17 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,9 +2,43 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
-template<typename T, int size>
+template <typename T>
+struct ITEM {
+    T data;
+    int prior;
+};
+
+template <typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
+ private:
+    ITEM<T> queue[size];
+    int begin, end;
+ public:
+    TPQueue() : begin(0), end(0) {}
+    void push(const T& data, int prior) {
+        int current = end;
+        while (current != begin && prior > queue[current - 1].prior) {
+            queue[current] = queue[current - 1];
+            if (--current < 0) {
+                current += size;
+            }
+        }
+        queue[current].data = data;
+        queue[current].prior = prior;
+        end = (end + 1) % size;
+    }
+    T pop() {
+        if (isEmpty()) {
+            throw std::out_of_range("Queue is empty");
+        }
+        if (--end < 0) {
+            end += size;
+        }
+        return queue[end].data;
+    }
+    bool isEmpty() const {
+        return begin == end;
+    }
 };
 
 struct SYM {
