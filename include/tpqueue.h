8038c4a3bfc34a4fbd79a6b9fb68e7e4a4afb381
krizal1 <159939607+krizal1@users.noreply.github.com>
2024-04-13 20:16:36 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,49 +1,56 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
-#include <string>
+
 #include <stdexcept>
 
 template<typename T, int size>
 class TPQueue {
  private:
+    struct QueueItem {
+        T data;
+        int prior;
+    };
+
+    QueueItem queue[size];
     int begin, end, count;
-    T* data;
 
  public:
-    TPQueue() : begin(0), end(0), count(0), data(new T[size]) {}
-
-    T pop() {
-        if (count == 0) {
-            throw std::string("Empty!!!");
-        } else {
-            count--;
-            T item = data[begin];
-            begin = (begin + 1) % size;
-            return item;
-        }
-    }
+    TPQueue() : begin(0), end(0), count(0) {}
 
     void push(const T& item) {
-        if (count >= size)
-            throw std::string("Full!!!");
-
-        int Index = end;
-        count++;
-
-        for (int i = begin; i < size; i++) {
-            if (data[i].prior < item.prior) {
-                Index = i;
-                break;
-            }
+        if (count >= size) {
+            throw std::out_of_range("Queue is full");
         }
-
-        for (int i = end; i > Index; i--) {
-            data[(i + 1) % size] = data[i % size];
+        if (count == 0) {
+            queue[end].data = item;
+            queue[end].prior = item.prior;
+            count++;
+            return;
         }
-
-        data[Index % size] = item;
+        int current = end;
+        while (current != begin && item.prior > queue[(current - 1 + size) % size].prior) {
+            queue[current] = queue[(current - 1 + size) % size];
+            current = (current - 1 + size) % size;
+        }
+        queue[current].data = item;
+        queue[current].prior = item.prior;
         end = (end + 1) % size;
+        count++;
+    }
+
+    T pop() {
+        if (isEmpty()) {
+            throw std::out_of_range("Queue is empty");
+        }
+        T result = queue[begin].data;
+        begin = (begin + 1) % size;
+        count--;
+        return result;
+    }
+
+    bool isEmpty() const {
+        return count == 0;
     }
 };
 
