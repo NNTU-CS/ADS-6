283287292701db2e137f75ffb5e484c4b9ffab70
krizal1 <159939607+krizal1@users.noreply.github.com>
2024-04-13 19:04:33 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,45 +1,49 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+#include <string>
 #include <stdexcept>
 
-template <typename T>
-struct ITEM {
-    T data;
-    int prior;
-};
-
-template <typename T, int size>
+template<typename T, int size>
 class TPQueue {
  private:
-    ITEM<T> queue[size];
-    int begin, end;
+    int begin, end, count;
+    T* data;
 
  public:
-    TPQueue() : begin(0), end(0) {}
-    void push(const T& data, int prior) {
-        int current = end;
-        while (current != begin && prior > queue[current - 1].prior) {
-            queue[current] = queue[current - 1];
-            if (--current < 0) {
-                current += size;
+    TPQueue() : begin(0), end(0), count(0), data(new T[size]) {}
+
+    T pop() {
+        if (count == 0) {
+            throw std::string("Empty!!!");
+        } else {
+            count--;
+            T item = data[begin];
+            begin = (begin + 1) % size;
+            return item;
+        }
+    }
+
+    void push(const T& item) {
+        if (count >= size)
+            throw std::string("Full!!!");
+
+        int Index = end;
+        count++;
+
+        for (int i = begin; i < size; i++) {
+            if (data[i].prior < item.prior) {
+                Index = i;
+                break;
             }
         }
-        queue[current].data = data;
-        queue[current].prior = prior;
+
+        for (int i = end; i > Index; i--) {
+            data[(i + 1) % size] = data[i % size];
+        }
+
+        data[Index % size] = item;
         end = (end + 1) % size;
-    }
-    T pop() {
-        if (isEmpty()) {
-            throw std::out_of_range("Queue is empty");
-        }
-        if (--end < 0) {
-            end += size;
-        }
-        return queue[end].data;
-    }
-    bool isEmpty() const {
-        return begin == end;
     }
 };
 
