a47c2dd92ada86814493c0f0fb1b400ed5cb7e01
FomenkovaAnastasia <160059344+FomenkovaAnastasia@users.noreply.github.com>
2024-04-14 13:52:10 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,42 @@
 
 template<typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
+ private:
+    SYM* arr;
+    int size, begin, end, k;
+
+public:
+    TPQueue(int s) : size(s), begin(0), end(0), k(0) {
+        arr = new SYM[size];
+    }
+
+    ~TPQueue() {
+        delete[] arr;
+    }
+
+    void push(SYM elem) {
+        if (k < size) {
+            int i = end - 1;
+            while (i >= begin && elem.prior > arr[i].prior) {
+                arr[i + 1] = arr[i];
+                i--;
+            }
+            arr[i + 1] = elem;
+            end++;
+            k++;
+        }
+    }
+
+    SYM pop() {
+        if (k > 0) {
+            k--;
+            return arr[begin++];
+        }
+    }
+
+    bool isEmpty() {
+        return k == 0;
+    }
 };
 
 struct SYM {
