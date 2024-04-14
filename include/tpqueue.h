a1146acfa3ce6713bfe56d93a22438b815696f61
IanaVatagina <vatagina.iana@gmail.com>
2024-04-14 18:56:59 +0300

    Update

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,9 +2,26 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
+#include <string>
+
 template<typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
+ private:
+    T *arr;
+    int begin, end, count;
+ public:
+    TPQueue(): begin(0), end(0), count(0), arr(new T[size]) {}
+    void push(const T& item) {
+
+    }
+    T pop() {
+        if (count == 0)
+            throw std::string("Empty!");
+        else {
+            count--;
+            return arr[(begin++) % size];
+        }
+    }
 };
 
 struct SYM {
