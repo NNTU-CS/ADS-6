fd523912f1f14a24e4571d954987d20c2e61c561
Iakovlev2005 <159990646+Iakovlev2005@users.noreply.github.com>
2024-04-15 08:43:01 +0300

    B71.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,40 +5,42 @@
 
 template<typename T, int size>
 class TPQueue {
-    T* Array;
-    int first, last, count;
+  T* Array;
+  int first, last, count;
 
-public:
-    TPQueue() : Array(new T[size]), first(0), last(0), count(0) {}
-    void push(const T& value) {
-        if (count == size)
-            throw std::string("FULL!");
-        ++count;
-        int mesto = last;
-        for (int i = first; i < last; i++)
-            if (Array[i].prior < value.prior) {
-                mesto = i;
-                break;
-            }
-        for (int i = last; i > mesto; i--)
-            Array[i % size] = Array[(i - 1) % size];
-        Array[mesto % size] = value;
-        ++last;
+ public:
+  TPQueue(): Array(new T[size]), first(0), last(0), count(0) {}
+  void push(const T & value) {
+    if (count == size)
+      throw std::string("FULL!");
+    ++count;
+    int mesto = last;
+    //находим нужную позицию
+    for (int i = first; i < last; i++)
+      if (Array[i].prior < value.prior) {
+        mesto = i;
+        break;
+      }
+    //сдвигаем
+    for (int i = last; i> mesto; i--)
+      Array[i % size] = Array[(i - 1) % size];
+    //вставляем value
+    Array[mesto % size] = value;
+    ++last;
+  }
+  T& pop() {
+    if (count == 0) {
+      throw std::string("EMPTY!");
+    } else {
+      count--;
+      return Array[first++ % size];
     }
-    T& pop() {
-        if (count == 0) {
-            throw std::string("EMPTY!");
-        }
-        else {
-            count--;
-            return Array[first++ % size];
-        }
-    }
+  }
 };
 
 struct SYM {
-    char ch;
-    int prior;
+  char ch;
+  int prior;
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
