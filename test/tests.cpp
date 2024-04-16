b4eb2ecda819133d8b8342807aebddff6e03532f
TailorOfEnbizaka <160338576+TailorOfEnbizaka@users.noreply.github.com>
2024-04-16 10:41:55 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -19,7 +19,10 @@ TEST(lab6, test1) {
 
     SYM c3 = pqueue.pop();
     SYM c4 = pqueue.pop();
-
+    std::cout << c1.ch << std::endl;
+    std::cout << c2.ch << std::endl;
+    std::cout << c3.ch << std::endl;
+    std::cout << c4.ch << std::endl;
     ASSERT_EQ(c1.ch, 'e');
     ASSERT_EQ(c2.ch, 'b');
     ASSERT_EQ(c3.ch, 'f');
@@ -32,6 +35,8 @@ TEST(lab6, test2) {
     pqueue.push(SYM{'b', 4});
     SYM c1 = pqueue.pop();
     SYM c2 = pqueue.pop();
+    std::cout << c1.ch << std::endl;
+    std::cout << c2.ch << std::endl;
     ASSERT_EQ(c1.ch, 'a');
     ASSERT_EQ(c2.ch, 'b');
 }
@@ -44,6 +49,9 @@ TEST(lab6, test3) {
     SYM c1 = pqueue.pop();
     SYM c2 = pqueue.pop();
     SYM c3 = pqueue.pop();
+    std::cout << c1.ch << std::endl;
+    std::cout << c2.ch << std::endl;
+    std::cout << c3.ch << std::endl;
     ASSERT_EQ(c1.ch, 'c');
     ASSERT_EQ(c2.ch, 'a');
     ASSERT_EQ(c3.ch, 'b');
