c2aba6bc07414960c7432cff20a9d05294bf07d6
DariaChileeva <dariachileeva@yandex.ru>
2024-04-17 19:46:25 +0300

    ads6

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,10 +1,45 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+#include <string>
 
 template<typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
+private:
+    T data [size];
+    int begin, end, counter;
+public:
+    TPQueue(): begin(0), end(0), counter(0) {}
+    void push(const T &item){
+        int i;
+        if (counter >= size) {
+            throw std::string("Full!");
+        } else {
+            if (counter == 0) {
+                data[begin] = item;
+                counter++;
+            } else {
+                for (i = end; i >= begin; i--) {
+                    if (item.prior > data[i % size].prior) {
+                        data[(i + 1) % size] = data[i % size];
+                    } else {
+                        break;
+                    }
+                }
+                end++;
+                data[(i + 1) % size] = item;
+                counter++;
+            }
+        }
+    }
+    T pop() {
+        if (counter == 0) {
+            throw std::string("Empty!");
+        } else {
+            counter--;
+            return data[begin ++ % size];
+        }
+    }
 };
 
 struct SYM {
