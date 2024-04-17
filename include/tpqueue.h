203d2790cbf9e9a13881dcd9e54f7f0138ef7b53
Viksss16 <160354641+Viksss16@users.noreply.github.com>
2024-04-18 02:29:25 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -10,24 +10,24 @@ private:
     T *data;
 public:
     TPQueue() : begin(0), end(0), count(0), data(new T[size]) {}
-    void push(const T & item) {
-        int i = end;
-        if (count >= size) {
-            throw std::string("is full");
-        }
-        count++;
-        while (begin < end) {
-            if (item.prior > data[begin].prior) {
-                end = begin;
-                break;
-            }
-            begin++;
-        }
-        for (; i > end; i--)
-            data[i % size] = data[(i - 1) % size];
-            data[end % size] = item;
-            end++;
-    }
+     void push(const T & item) {
+      int end1 = end, begin1 = begin, i = end;
+      if (count >= size) {
+          throw std::string("is full");
+      }
+      count++;
+      while (begin1 < end) {
+          if (item.prior >= data[begin1].prior) {
+              end1 = begin1;
+              break;
+          }
+          begin1++;
+      }
+      for (; i > end1; i--)
+          data[i % size] = data[(i - 1) % size];
+          data[end1 % size] = item;
+          end++;
+  }
   
     T pop() {
         if (count == 0) {
