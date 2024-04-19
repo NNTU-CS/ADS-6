a93aac377f46627f6a1023f3af6921d47a641149
polinamoskvina <160390879+polinamoskvina@users.noreply.github.com>
2024-04-19 19:29:21 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,45 +1,47 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+#include <string>
 
 template<typename T, int size>
 class TPQueue {
  private:
-    T arr[5];
-    int c, e;
+    T *arr;
+    int top, end;
+    int c;
 
  public:
-    TPQueue() {
-        for (int i = 0; i < size; i++) {
-            arr[i].ch = 0;
-            arr[i].prior = -10;
-        }
-    }
-    void push(T sym) {
-        if (e != 0) {
-                int i = e - 1;
-                for (; i >= c; i--) {
-                    if (sym.prior > arr[i].prior) {
-                        arr[i + 1] = arr[i];
+    TPQueue() : arr(new T[size]), top(0), end(0), c(0) {}
+    void push(const T &i) {
+        int i;
+        if (c >= size) {
+            throw std::string("Is full");
+        } else {
+            if (c == 0) {
+                arr[top] = i;
+                c++;
+            } else {
+                for (i = end; i >= top; i--) {
+                    if (i.prior > arr[i % size].prior) {
+                        arr[(i + 1) % size] = arr[i % size];
                     } else {
                         break;
                     }
                 }
-                arr[++i] = sym;
-                e++;
-            } else {
-                arr[0] = sym;
-                e++;
+                end++;
+                arr[(i + 1) %size] = i;
+                c++;
             }
         }
-        T pop() {
-            T temp = arr[0];
-            for (int i = c; i < e - 1; i++) {
-                arr[i] = arr[i + 1];
-            }
-            e--;
-            return temp;
-     }
+    }
+    T pop() {
+        if (c == 0) {
+            throw std::string("Is empty");
+        } else {
+            c--;
+            return arr[top++ % size];
+        }
+    }
 };
 
 struct SYM {
