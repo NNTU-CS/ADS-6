ebe582bc2ab5ed21af7d3add38ce4386f532c224
MayaGerasimenko <160390629+MayaGerasimenko@users.noreply.github.com>
2024-04-19 17:54:47 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,9 +2,43 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
-template<typename T, int size>
+template<typename T, const int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
+  T arr[5];
+  int k = 0;
+  int end = 0;
+
+ public:
+  TPQueue() {
+    for (int i = 0; i < size; i++) {
+      arr[i].ch = 0;
+      arr[i].prior = -10;
+    }
+  }
+  void push(T sym) {
+    if (end != 0) {
+      for (int i = k - 1; i >= k; i--) {
+        if (sym.prior > arr[i].prior) {
+          arr[i + 1] = arr[i];
+        } else {
+          break;
+        }
+      }
+      arr[++i] = sym;
+      end++;
+    } else {
+      arr[0] = sym;
+      end++;
+    }
+  }
+  T pop() {
+    T f = arr[0];
+    for (int i = k; i < end - 1; i++) {
+      arr[i] = arr[i + 1];
+    }
+    end--;
+    return f;
+  }
 };
 
 struct SYM {
