0fe72e17f3a75c2afba8037e306e6c38e7dc6046
Funibi <139475082+Funibi@users.noreply.github.com>
2024-04-20 11:55:21 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,6 +2,8 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
+#include <string>
+
 template <typename T, int size>
 class TPQueue {
  private:
@@ -11,6 +13,7 @@ class TPQueue {
  public:
   TPQueue() : a(new T[size]), begin(0), end(0), c(0) {}
   void push(const T& t) {
+    if (c >= size) throw std::string("error");
     c++;
     int p = end;
     for (int i = end; i >= begin; i--) {
@@ -27,6 +30,8 @@ class TPQueue {
     if (c != 0) {
       c--;
       return a[begin++ % size];
+    } else {
+      throw std::string("error");
     }
   }
 };
