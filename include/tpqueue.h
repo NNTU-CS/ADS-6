810f57fdd2dcc2288d79657a5f9dc4ac0578cda4
AntiCreep753 <160477169+AntiCreep753@users.noreply.github.com>
2024-04-20 12:01:46 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,42 +1,52 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
-#include <string>
 
-template<typename T, int size>
+template <typename T, int size>
 class TPQueue {
-  private:
-    int head, tail;
-    int counter;
-    T *data;
+ private:
+  T data[10] = {};
+  int index = 0, start = 0;
 
  public:
-    TPQueue() : head(0), tail(0), counter(0), data(new T[size]) {}
-    void push(const T &item) {
-        if (counter >= size)
-            throw std::string("Full");
-        counter++;
-        int t1 = tail, h1 = head;
-        while (h1 < tail) {
-            if (data[h1].prior < item.prior) {
-                t1 = h1;
-                break;
-            }
-            h1++;
-        }
-        for (int i = tail; i > t1; i--)
-            data[i % size] = data[(i - 1) % size];
-        data[t1 % size] = item;
-        tail++;
+  int get_size() { return index - start; }
+
+  bool is_empty() {
+    if (this->get_size() == 0) {
+      return true;
+    } else {
+      return false;
     }
-    T pop() {
-        if (counter == 0) {
-            throw std::string("Empty");
-        } else {
-            counter--;
-            return data[(head++) % size];
-        }
+  }
+
+  bool is_full() {
+    if (this->get_size() != 0) {
+      return true;
+    } else {
+      return false;
     }
+  }
+
+  T pop() {
+    start += 1;
+    return data[start - 1];
+  }
+
+  void push(T elem) {
+    data[index] = elem;
+    index += 1;
+    for (int i = start; i < index; i++) {
+      for (int j = start; j < index - 1; j++) {
+        if (data[j].prior < data[j + 1].prior) {
+          T boofer = data[j];
+          data[j] = data[j + 1];
+          data[j + 1] = boofer;
+        }
+      }
+    }
+  }
+
+  T get_elem_by_id(int id) { return data[id]; }
 };
 
 struct SYM {
