f3ec1e4077f6e7ce86d5cfc225f3147147e0f7ce
arinamalygina <160202011+arinamalygina@users.noreply.github.com>
2024-04-20 14:16:11 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,10 +1,43 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+#include <string>
 
 template<typename T, int size>
 class TPQueue {
   // реализация шаблона очереди с приоритетом на кольцевом буфере
+ private:
+  T* arr;
+  int begin, end, count;
+
+ public:
+  TPQueue() : begin(0), end(0), count(0) { arr = new T[size]; }
+
+  ~TPQueue() { delete[] arr; }
+
+  void push(const T& data) {
+    if (count >= size) throw std::string("Full");
+    count++;
+    int pos = end;
+    for (int i = begin; i < end; i++) {
+      if (arr[i].prior < data.prior) {
+        pos = i;
+        break;
+      }
+    }
+    for (int i = end - 1; i >= pos; i--) arr[(i + 1) % size] = arr[i % size];
+    arr[pos % size] = data;
+    end++;
+  }
+
+  T pop() {
+    if (count == 0) {
+      throw std::string("Empty");
+    } else {
+      count--;
+      return arr[begin++ % size];
+    }
+  }
 };
 
 struct SYM {
