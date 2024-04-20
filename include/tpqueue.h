06b250486fe13f9aeb314d72c7b2f379fd17f210
KatyaTsybrova <160347264+KatyaTsybrova@users.noreply.github.com>
2024-04-20 13:29:25 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,29 +2,29 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
-template<typename T, int size>
+template <typename T, int size>
 class TPQueue {
-private:
-T data[18] = {};
-int index = 0, start = 0;
-public:
-T pop() {
- start += 1;
- return data[start - 1];
-}
-void push(T elem) {
- data[index] = elem;
- index += 1;
- for (int t = start; t < index; t++) {
-  for (int i = start; i < index - 1; i++) {
-   if ((data[i + 1].prior) > (data[i].prior)) {
-    T b = data[i];
-    data[i] = data[i + 1];
-    data[i + 1] = b;
-   }
+ private:
+  T data[18] = {};
+  int index = 0, start = 0;
+ public:
+  T pop() {
+    start += 1;
+    return data[start - 1];
   }
- }
-}
+  void push(T elem) {
+    data[index] = elem;
+    index += 1;
+    for (int t = start; t < index; t++) {
+      for (int i = start; i < index - 1; i++) {
+        if ((data[i + 1].prior) > (data[i].prior)) {
+          T b = data[i];
+          data[i] = data[i + 1];
+          data[i + 1] = b;
+        }
+      }
+    }
+  }
 };
 struct SYM {
   char ch;
