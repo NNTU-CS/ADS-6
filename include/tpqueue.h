ed5bd35d57548daddc4ceb606b3e7b5fa421078d
Arina-Arahis <159996104+Arina-Arahis@users.noreply.github.com>
2024-04-20 23:19:42 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,55 +1,49 @@
-// include/tpqueue.h
+// Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
-
+#include <iostream>
 #include <string>
 
 template<typename T, int size>
 class TPQueue {
  private:
-    T* data;
-    int head, tail, counter;  // изменение имен переменных
+  T* data;
+  int begin, end, count;
 
  public:
-    TPQueue() : head(0), tail(0), counter(0), data(new T[size]) {}
-
-    ~TPQueue() {
-        delete[] data;
-    }
-
-    void push(const T& item) {  // изменение имени метода
-        if (counter >= size) {
-            throw std::string("Full");
-        } else {
-            counter++;
-            int pos = tail, i = head;  // изменение имен переменных
-            while (i < tail) {
-                if (data[i].prior < item.prior) {
-                    pos = i;
-                    break;
-                }
-                i++;
-            }
-            for (int i = tail; i > pos; i--)
-                data[i % size] = data[(i - 1) % size];
-            data[pos % size] = item;
-            tail++;
+  TPQueue(): begin(0), end(0), count(0), data(new T[size]) {}
+  void push(const T& item) {
+    if (count >= size) {
+      throw std::string("Empty");
+    } else {
+      count++;
+      int pos = end, i = begin;
+      while (i < end) {
+        if (data[i].prior < item.prior) {
+          pos = i;
+          break;
         }
+        i++;
+      }
+      for (int i = end; i > pos; i--)
+        data[i % size] = data[(i - 1) % size];
+      data[pos % size] = item;
+      end++;
     }
-
-    T pop() {  // изменение имени метода
-        if (counter == 0) {
-            throw std::string("Empty");
-        } else {
-            counter--;
-            return data[(head++) % size];
-        }
+  }
+  T pop() {
+    if (count == 0) {
+      throw std::string("Empty!");
+    } else {
+      count--;
+      return data[(begin++) % size];
     }
+  }
 };
 
 struct SYM {
-    char ch;
-    int prior;
+  char ch;
+  int prior;
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
