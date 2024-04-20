e3f6d2e8cecc5c38b5998d0ce95d359375be7a4b
Arina-Arahis <159996104+Arina-Arahis@users.noreply.github.com>
2024-04-20 22:44:09 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,42 +5,42 @@
 template<typename T, int size>
 class TPQueue {
   // реализация шаблона очереди с приоритетом на кольцевом буфере
- private:
+  private:
     int front, rear; // измененные имена переменных
     int counter;
     T *data;
 
- public:
+  public:
     TPQueue() : front(0), rear(0), counter(0), data(new T[size]) {}
     void push(const T &item) {
-        if (counter >= size)
-            throw std::string("Full");
-        counter++;
-        int t1 = rear, h1 = front;
-        while (h1 < rear) {
-            if (data[h1].prior < item.prior) {
-                t1 = h1;
-                break;
-            }
-            h1++;
+      if (counter >= size)
+        throw std::string("Full");
+      counter++;
+      int t1 = rear, h1 = front;
+      while (h1 < rear) {
+        if (data[h1].prior < item.prior) {
+          t1 = h1;
+          break;
         }
-        for (int i = rear; i > t1; i--)
-            data[i % size] = data[(i - 1) % size];
-        data[t1 % size] = item;
-        rear++;
+        h1++;
+      }
+      for (int i = rear; i > t1; i--)
+        data[i % size] = data[(i - 1) % size];
+      data[t1 % size] = item;
+      rear++;
     }
-    T pop() {
-        if (counter == 0) {
-            throw std::string("Empty");
-        } else {
-            counter--;
-            return data[(front++) % size];
-        }
-    }
+T pop() {
+  if (counter == 0) {
+    throw std::string("Empty");
+  } else {
+    counter--;
+    return data[(front++) % size];
+  }
+}
 };
 
 struct SYM {
-  char ch;
-  int prior;
+char ch;
+int prior;
 };
 #endif  // INCLUDE_TPQUEUE_H_
