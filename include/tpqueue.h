1b9df9ed1934aebec7ffce72291c42a85e3132e0
EgorKr08 <Kryukovegor08@gmail.com>
2024-04-20 12:56:21 +0300

    !

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,58 +1,46 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
-
-#include <stdexcept>
+#include <string>
 
 template<typename T, int size>
 class TPQueue {
  private :
-    int head, tail;
-    int count;
-    T *data;
+    int front, rear;
+    int itemCount;
+    T *storage;
 
  public :
-    TPQueue() : head(0), tail(0), count(0), data(new T[size]) {
+    TPQueue() : front(0), rear(0), itemCount(0), storage(new T[size]) {
     }
 
     T pop() {
-        if (count == 0) {
-            throw std::out_of_range("Queue is empty");
+        if (itemCount == 0) {
+            throw std::string("Queue is empty");
         } else {
-            int highest_priority_index = head;
-            for (int i = head + 1; i < tail; ++i) {
-                if (data[i % size].prior <
-                    data[highest_priority_index % size].prior) {
-                    highest_priority_index = i;
-                }
-            }
-            T highest_priority_item = data[highest_priority_index % size];
-
-            for (int i = highest_priority_index; i < tail - 1; ++i) {
-                data[i % size] = data[(i + 1) % size];
-            }
-
-            --tail;
-            --count;
-
-            return highest_priority_item;
+            itemCount--;
+            return storage[(front + 1) % size];
+            return storage[(front++) % size];
         }
     }
 
     void push(const T &item) {
-        if (count >= size) {
-            throw std::out_of_range("Queue is full");
+        if (itemCount >= size) {
+            throw std::string("Queue is full");
         }
-
-        int insert_index = tail;
-        while (insert_index > head && data[(insert_index - 1) % size].prior > item.prior) {
-            data[insert_index % size] = data[(insert_index - 1) % size];
-            --insert_index;
+        itemCount++;
+        int insertIndex = rear, compareIndex = front;
+        while (compareIndex < rear) {
+            if (storage[compareIndex].prior < item.prior) {
+                insertIndex = compareIndex;
+                break;
+            }
+            compareIndex++;
         }
-
-        data[insert_index % size] = item;
-        ++tail;
-        ++count;
+        for (int i = rear; i > insertIndex; i--)
+            storage[i % size] = storage[(i - 1) % size];
+        storage[insertIndex % size] = item;
+        rear++;
     }
 };
 
@@ -60,5 +48,4 @@ struct SYM {
     char ch;
     int prior;
 };
-
 #endif  // INCLUDE_TPQUEUE_H_
