09a5b5b878146e7a1dd3f98c6b11e5944b754fe9
smirnovaalin <160322763+smirnovaalin@users.noreply.github.com>
2024-04-20 11:41:17 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -10,37 +10,35 @@ int prior;
 template<typename T, int size>
 class TPQueue {
  private:
-T* arr; 
-int item; 
-public:
- TPQueue() :  item(0), arr(new T[size]) {};
- ~TPQueue() { delete[] arr; };
- void push(const T&elm) {
-  SYM temp = (SYM)elm;
-  if (arr == 0) {
-   item++;
-  }
-  else if (item != size) {
-   int pos = 0;
-   for (int i = item; i > 0; i--) {
-    if (((SYM)arr[i - 1]).prior < temp.prior) {
-     pos = i;
-     break;
-    }
-    arr[i] = arr[i - 1];
+T* arr;
+int item;
+ public:
+TPQueue() :  item(0), arr(new T[size]) {};
+~TPQueue() { delete[] arr; }
+void push(const T&elm) {
+ SYM temp = (SYM)elm;
+ if (arr == 0) {
+  item++;
+ } else if (item != size) {
+  int pos = 0;
+  for (int i = item; i > 0; i--) {
+   if (((SYM)arr[i - 1]).prior < temp.prior) {
+    pos = i;
+    break;
    }
-   arr[pos] = elm;
-   item++;
+   arr[i] = arr[i - 1];
   }
+  arr[pos] = elm;
+  item++;
  }
- T pop() {
-  if (item == 0) {
-   return T();
-  }
-  else {
-   return arr[--item];
-  }
+}
+T pop() {
+ if (item == 0) {
+  return T();
+ } else {
+  return arr[--item];
  }
+}
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
