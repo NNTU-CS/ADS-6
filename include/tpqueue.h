e465c02855b77086ed726d806f0f3da7f9786454
flapik19 <selivanovvlad3211@gmail.com>
2024-04-20 11:37:31 +0300

    try

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,9 +2,45 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
-template<typename T, int size>
+#include <stdexcept>
+template <typename T, int size> 
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
+private:
+    T data[size];
+    int count;
+    int begin, end;
+public:
+    TPQueue() : count(0), begin(0), end(0) {}
+
+    void push(const T& elem, int prior) {
+        if (count < size) {
+            int pos = end;
+            while (pos > begin && prior > data[(pos-1) % size].prior) {
+                data[pos % size] = data[(pos-1) % size];
+                pos--;
+            }
+            data[pos % size] = elem;
+            end = (end + 1) % size;
+            count++;
+        } else {
+            throw std::out_of_range("queue is full");
+        }
+    }
+
+    T pop() {
+        if (!isEmpty()) {
+            T elem = data[begin];
+            begin = (begin + 1) % size;
+            count--;
+            return elem;
+        } else {
+            throw std::out_of_range("queue is empty");
+        }
+    }
+
+    bool isEmpty() {
+        return (count == 0);
+    }
 };
 
 struct SYM {
@@ -12,4 +48,4 @@ struct SYM {
   int prior;
 };
 
-#endif  // INCLUDE_TPQUEUE_H_
+#endif // INCLUDE_TPQUEUE_H_
