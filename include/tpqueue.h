cdd0a88eabf3344b959d4421db4819cdc0958027
alinaavv <160399232+alinaavv@users.noreply.github.com>
2024-04-20 20:04:14 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,11 +2,58 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
+template<typename T>
+struct ITEM {
+    T data; 
+    int prior; 
+    ITEM<T>* next; 
+};
 template<typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
+ private:
+    ITEM<T>* head; 
+    ITEM<T>* tail;
+public:
+    TPQueue() : head(nullptr), tail(nullptr) {}
+    ~TPQueue() {
+        while (head) {
+            ITEM<T>* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+    void push(const T& data, int prior) {
+        ITEM<T>* temp = new ITEM<T>;
+        temp->data = data;
+        temp->prior = prior;
+        temp->next = nullptr;
+    if (!head || prior < head->prior) {
+            if (!head) 
+                tail = temp;
+            temp->next = head;
+            head = temp;
+        } else {
+            ITEM<T>* current = head;
+            while (current->next && current->next->prior <= prior) {
+                current = current->next;
+            }
+            temp->next = current->next;
+            current->next = temp;
+            if (!temp->next)
+                tail = temp;
+        }
+    }
+    T pop() {
+        if (!head) {
+            throw std::out_of_range("Queue is empty");
+        }
+        T data = head->data;
+        ITEM<T>* temp = head;
+        head = head->next;
+        delete temp;
+        return data;
+    }
 };
-
 struct SYM {
   char ch;
   int prior;
