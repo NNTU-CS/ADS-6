c0643789105574d24539ffdda35813f89de12c4b
artemkosterin <160392965+artemkosterin@users.noreply.github.com>
2024-04-20 12:52:35 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,44 @@
 
 template<typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
+  private:
+    T* arr;
+    size_t capacity;
+    size_t count;
+
+public:
+    TPQueue() : arr(new T[size]), capacity(size), count(0) {}
+
+    ~TPQueue() {
+        delete[] arr;
+    }
+
+    void push(const T& elem) {
+        if (count == capacity) {
+            throw std::out_of_range("Queue is full");
+        }
+
+        size_t i = count;
+        while (i > 0 && arr[i - 1].prior >= elem.prior) {
+            arr[i] = arr[i - 1];
+            --i;
+        }
+        arr[i] = elem;
+        ++count;
+    }
+
+    T pop() {
+        if (count == 0) {
+            throw std::out_of_range("Queue is empty");
+        }
+
+        T tmp = arr[0];
+        for (size_t i = 1; i < count; ++i) {
+            arr[i - 1] = arr[i];
+        }
+        --count;
+        return tmp;
+    }
 };
 
 struct SYM {
