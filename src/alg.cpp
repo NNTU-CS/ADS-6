de393392dc3e3b2aae9b73b3585b814e94aaca1d
Arina-Arahis <159996104+Arina-Arahis@users.noreply.github.com>
2024-04-20 23:08:07 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,48 +1,49 @@
+// Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
-
+#include <iostream>
 #include <string>
 
 template<typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
  private:
-    int begin, end, count;
-    T *data;
+  T* data;
+  int begin, end, count;
 
  public:
-    TPQueue() : begin(0), end(0), count(0), data(new T[size]) {}
-
-    T pop() {
-        if (count == 0) {
-            throw std::string("Empty!!!");
-        } else {
-            count--;
-            return data[begin++ % size];
+  TPQueue(): begin(0), end(0), count(0), data(new T[size]) {}
+  void push(const T& item) {
+    if (count >= size) {
+      throw std::string("Empty");
+    } else {
+      count++;
+      int pos = end, i = begin;
+      while (i < end) {
+        if (data[i].prior < item.prior) {
+          pos = i;
+          break;
         }
+        i++;
+      }
+      for (int i = end; i > pos; i--)
+        data[i % size] = data[(i - 1) % size];
+      data[pos % size] = item;
+      end++;
     }
-
-    void push(const T &item) {
-        if (count >= size)
-            throw std::string("Full!!!");
-        count++;
-        int Index = end;
-        for (int i = begin; i < end; i++) {
-            if (data[i].prior < item.prior) {
-                Index = i;
-                break;
-            }
-        }
-        for (int i = end; i > Index; i--)
-            data[i % size] = data[(i - 1) % size];
-        data[Index % size] = item;
-        end++;
+  }
+  T pop() {
+    if (count == 0) {
+      throw std::string("Empty!");
+    } else {
+      count--;
+      return data[(begin++) % size];
     }
+  }
 };
 
 struct SYM {
-    char ch;
-    int prior;
+  char ch;
+  int prior;
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
