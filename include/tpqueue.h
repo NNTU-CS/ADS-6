bffc98137cd6037c0f9b721314d3718d6652d20c
flapik19 <selivanovvlad3211@gmail.com>
2024-04-20 11:55:30 +0300

    idk

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -3,38 +3,39 @@
 #define INCLUDE_TPQUEUE_H_
 
 #include <stdexcept>
-template <typename T, int size> 
+template<typename T, int size>
 class TPQueue {
 private:
-    T data[size];
-    int count;
-    int begin, end;
+ T data[size];
+ int count;
+ int begin;
+ int end;
 public:
-    TPQueue() : count(0), begin(0), end(0) {}
+ TPQueue () : count(0), begin(0), end(0) {}
 
-    void push(const T& elem, int prior) {
-        if (count < size) {
-            int pos = end;
-            while (pos > begin && prior > data[(pos-1) % size].prior) {
-                data[pos % size] = data[(pos-1) % size];
-                pos--;
-            }
-            data[pos % size] = elem;
-            end = (end + 1) % size;
-            count++;
-        } else {
-            throw std::out_of_range("queue is full");
+ void push(const T& elem) { 
+    if (count < size) {
+        int pos = end;
+        while (pos > begin && elem > data[(pos - 1) % size]) {
+            data[pos % size] = data[(pos - 1) % size];
+            pos--;
         }
+        data[pos % size] = elem;
+        end = (end + 1) % size;
+        count ++;
+    } else { 
+        throw std::out_of_range("queue is full");
     }
+ }
 
-    T pop() {
+ T pop() {
         if (!isEmpty()) {
             T elem = data[begin];
             begin = (begin + 1) % size;
             count--;
             return elem;
         } else {
-            throw std::out_of_range("queue is empty");
+            throw std::out_of_range("Queue is empty");
         }
     }
 
@@ -46,6 +47,10 @@ public:
 struct SYM {
   char ch;
   int prior;
+
+  bool operator>(const SYM& other){
+    return prior > other.prior;
+  }
 };
 
-#endif // INCLUDE_TPQUEUE_H_
+#endif  // INCLUDE_TPQUEUE_H_
