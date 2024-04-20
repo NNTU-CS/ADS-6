1bdcd6b164381c40e94d23ddc0cde5f4bf73db8d
artemkosterin <160392965+artemkosterin@users.noreply.github.com>
2024-04-20 13:08:19 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,12 +2,15 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
+#include <stdexcept>
+#include <cstddef>
+
 template<typename T, int size>
 class TPQueue {
  private:
     T* arr;
-    size_t capacity;
-    size_t count;
+    std::size_t capacity;
+    std::size_t count;
 
  public:
     TPQueue() : arr(new T[size]), capacity(size), count(0) {}
@@ -21,7 +24,7 @@ class TPQueue {
             throw std::out_of_range("Queue is full");
         }
 
-        size_t i = count;
+        std::size_t i = count;
         while (i > 0 && arr[i - 1].prior >= elem.prior) {
             arr[i] = arr[i - 1];
             --i;
@@ -35,10 +38,7 @@ class TPQueue {
             throw std::out_of_range("Queue is empty");
         }
 
-        T tmp = arr[0];
-        for (size_t i = 1; i < count; ++i) {
-            arr[i - 1] = arr[i];
-        }
+        T tmp = arr[count - 1];
         --count;
         return tmp;
     }
