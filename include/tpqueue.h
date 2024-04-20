0186471007a6c15b57780c82ff99c7c20db58100
EgorKr08 <Kryukovegor08@gmail.com>
2024-04-20 13:44:05 +0300

    !

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -3,53 +3,51 @@
 #define INCLUDE_TPQUEUE_H_
 #include <string>
 
-template<typename T, int size>
+template<typename T, int Size>
 class TPQueue {
-   private :
-      int front, rear, itemCount;
-  T *storage;
+ private :
+    T data[Size];
+    int front, rear, itemCount;
 
-   public :
-      TPQueue() : front(0), rear(0), itemCount(0), storage(new T[size]) {
-  }
+ public :
+    TPQueue() : front(0), rear(0), itemCount(0) {
+    }
 
-  void push(const T &item) {
-    int currentNum;
-    if (itemCount >= size) {
-      throw std::string("Queue is full");
-    } else {
-      if (itemCount == 0) {
-        storage[front] = item;
-        itemCount++;
-      } else {
-        for (currentNum = rear; currentNum >= front; currentNum--) {
-          if (item.prior > storage[currentNum % size].prior) {
-            storage[(currentNum + 1) % size] = storage[currentNum % size];
-          } else {
-            break;
-          }
+    void push(const T &item) {
+        int currentSum;
+        if (itemCount >= Size) {
+            throw std::string("Full");
+        } else {
+            if (itemCount == 0) {
+                data[front] = item;
+               itemCount++;
+            } else {
+                for (currentSum = rear; currentSum >= front; currentSum--) {
+                    if (item.prior > data[currentSum % Size].prior) {
+                        data[(currentSum + 1) % Size] = data[currentSum % Size];
+                    } else {
+                        break;
+                    }
+                }
+                rear++;
+                data[(currentSum + 1) % Size] = item;
+                itemCount++;
+            }
         }
-        rear++;
-        storage[(currentNum + 1) % size] = item;
-        itemCount++;
-      }
     }
-  }
 
-  T pop() {
-    if (itemCount == 0) {
-      throw std::string("Queue is empty");
-    } else {
-      itemCount--;
-      return storage[(front + 1) % size];
-      return storage[(front++) % size];
+    T pop() {
+        if (itemCount == 0) {
+            throw std::string("Empty");
+        } else {
+            itemCount--;
+            return data[front++ % Size];
+        }
     }
-  }
 };
 
 struct SYM {
-  char ch;
-  int prior;
+    char ch;
+    int prior;
 };
-
 #endif  // INCLUDE_TPQUEUE_H_
