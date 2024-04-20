1c5ab6ea67a982842ccde75ec6115f23950cd8a5
Shaybye <159885796+Shaybye@users.noreply.github.com>
2024-04-20 16:17:31 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,9 +4,44 @@
 
 template<typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
-};
+  private:
+    T* arr;
+    int SIZE;
+    int begin, end, count;
 
+  public:
+    TQueue(int sizeQueue) {
+        SIZE = sizeQueue;
+        begin = 0;
+        end = 0;
+        count = 0;
+        arr = new T[SIZE];
+    }
+    ~TQueue() {
+        delete[] arr;
+    }
+    void push(const T& item) {
+        assert(count < size);
+        count++;
+        int pe = end;
+        for (int i = begin; i < end; i++) {
+            if (arr[i].prior < item.prior) {
+                pe = i;
+                break;
+            }
+        }
+        for (int i = end - 1; i >= pos; i--) {
+            arr[(i+1) % size] = arr[i % size]
+        }
+        arr[pe % size] = item;
+        end++;
+    }
+    T pop() {
+        assert(count > 0);
+        count--;
+        return arr[begin++ % size];
+    }
+};
 struct SYM {
   char ch;
   int prior;
