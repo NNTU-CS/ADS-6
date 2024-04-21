5ef88adfad16fe0f5781b2e5dbf9e33c3d71a234
BaevGeorgy55 <160393440+BaevGeorgy55@users.noreply.github.com>
2024-04-21 20:20:41 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,35 @@
 
 template<typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
+  private:
+    T* mass;
+    std::size_t capacity;
+    std::size_t count;
+  public:
+    TPQueue() : mass(new T[size]), capacity(size), count(0) {}
+    ~TPQueue() {
+        delete[] mass;
+    }
+    void push(const T& elem) {
+        if (count == capacity) {
+            throw std::out_of_range("Queue is full");
+        }
+        std::size_t i = count;
+        while (i > 0 && mass[i - 1].prior >= elem.prior) {
+            mass[i] = mass[i - 1];
+            --i;
+        }
+        mass[i] = elem;
+        ++count;
+    }
+    T pop() {
+        if (count == 0) {
+            throw std::out_of_range("Queue is empty");
+        }
+        T tmp = mass[count - 1];
+        --count;
+        return tmp;
+    }
 };
 
 struct SYM {
