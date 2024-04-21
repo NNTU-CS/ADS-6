cb82f4f8079489f3ebde9b73a1d46fe294d0af84
nrtcru <94432719+nrtcru@users.noreply.github.com>
2024-04-21 21:04:09 +0300

    b54 update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,48 +1,38 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+#include <string>
 
-template<typename T>
+template<typename T, int size>
 class TPQueue {
- private:
-  struct Item {
-    T item;
-    Item* next;
-  };
-  Item* head;
-  Item* tail;
+  T* arr;
+  int first, last, count;
 
  public:
-    TPQueue(): head(nullptr), tail(nullptr) {}
-    void push(const T& value) {
-      Item* temp = new Item;
-      temp->item = value;
-      temp->next = nullptr;
-      if (head == nullptr) {
-        head = temp;
-        tail = temp;
-        return;
+  TPQueue(): arr(new T[size]), first(0), last(0), count(0) {}
+  void push(const T & data) {
+    if (count == size)
+      throw std::string("FULL!");
+    ++count;
+    int pos = last;
+    for (int i = first; i < last; i++)
+      if (arr[i].prior < data.prior) {
+        pos = i;
+        break;
       }
-      if (head->item.prior < value.prior) {
-        temp->next = head;
-        head = temp;
-        return;
-      }
-      Item* i = head;
-      while (i->next != nullptr && i->next->item.prior >= value.prior)
-        i = i->next;
-      temp->next = i->next;
-      i->next = temp;
-      if (temp->next == nullptr)
-        tail = temp;
+    for (int i = last; i> pos; i--)
+      arr[i % size] = arr[(i - 1) % size];
+    arr[pos % size] = data;
+    ++last;
+  }
+  T& pop() {
+    if (count == 0) {
+      throw std::string("EMPTY!");
+    } else {
+      count--;
+      return arr[first++ % size];
     }
-    const T pop() {
-      Item* temp = head;
-      T res = temp->item;
-      head = head->next;
-      delete temp;
-      return res;
-    }
+  }
 };
 
 struct SYM {
