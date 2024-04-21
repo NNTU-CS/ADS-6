38a0c322ca65543c09ad2f39d047cccfd89bb0f0
MariaSadkova <160345524+MariaSadkova@users.noreply.github.com>
2024-04-21 16:31:02 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,64 +2,43 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
-#include <iostream>
 template<typename T, int size>
 class TPQueue {
-private:
-    T* arr;
-    int maxSize;
-    int currentSize;
+    T arr[5];
+    int k;
 
-public:
-    TPQueue() {
-        arr = new T[size];
-        maxSize = size;
-        currentSize = 0;
-    }
-
+ public:
+    TPQueue() : k(0), arr(new T[size]) {}
     ~TPQueue() {
-        delete[] arr;
-    }
-
-    void push(const T& item, int priority) {
-        if (currentSize < maxSize) {
-            int i = currentSize - 1;
-            while (i >= 0 && priority > arr[i].prior) {
-                arr[i + 1] = arr[i];
-                i--;
-            }
-            arr[i + 1] = item;
-            currentSize++;
-        }
-        else {
-            std::cout << "Queue is full" << std::endl;
+        if (k >= 0) {
+            delete[] arr;
         }
     }
-
+    void push(const T& b) {
+        int p = 0;
+        while (p < k) {
+            if (arr[p].prior < b.prior) break;
+            p++;
+        }
+        T t1 = arr[p];
+        arr[p] = b;
+        for (int i = p + 1; i < k + 1; i++) {
+            T t2 = arr[i];
+            arr[i] = t1;
+            t1 = t2;
+        }
+        k++;
+    }
     T pop() {
-        if (currentSize > 0) {
-            T item = arr[0];
-            for (int i = 0; i < currentSize - 1; i++) {
-                arr[i] = arr[i+1];
-            }
-            currentSize--;
-            return item;
+        if (!k) throw std::string("Empty");
+        T b = arr[0];
+        k--;
+        for (int i = 0; i < k; i++) {
+            arr[i] = arr[i + 1];
         }
-        else {
-            std::cout << "Queue is empty" << std::endl;
-            return T();
-        }
-    }
-
-    bool isEmpty() {
-        return currentSize == 0;
-    }
-
-    bool isFull() {
-        return currentSize == maxSize;
+        return b;
     }
 };
-
 struct SYM
 {
 	char ch;
