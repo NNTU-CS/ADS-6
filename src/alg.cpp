28dd5ee4cd940a7bdf9dad7411348afd0e8055e0
maxim.sustaev <maxim.sustaev@yandex.ru>
2024-04-21 20:54:47 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,31 +1,68 @@
 // Copyright 2021 NNTU-CS
-#include <list>
+
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <utility>
+using namespace std;
+
+struct SYM {
+    char ch;
+    int prior;
+};
 
 template<typename T, int size>
 class TPQueue {
 private:
-  std::list<T> Query;
+    T * arr;
+    int head;
+    int count = 0;
 public:
-  void push(T a) {
-    if (!Query.empty()) {
-      for (auto i = Query.cbegin(); i != Query.cend(); ++i) {
-        if (a.prior <= (*i).prior) {
-          Query.insert(i, a);
-          return;
-        }
-      }
+    TPQueue() {
+        arr = new T[size + 1];
+        head = -1;
     }
-    Query.push_back(a);
-  }
+    void push(T value) {
+        if (head == -1) {
+            arr[++head] = value;
+        }
+        else if (head == 0) {
+            if (arr[head].prior < value.prior) {
+                arr[++head] = value;
+            }
+            else {
+                arr[++head] = value;
+                swap(arr[0], arr[head]);
+            }
+        }
+        else {
+            count += 1;
+            arr[++head] = value;
+            for (int i = 0; i < count + 1; ++i) {
+                for (int j = 0; j < count + 1; ++j) {
+                    if (arr[j].prior > arr[j + 1].prior) {
+                        swap(arr[j], arr[j + 1]);
+                    }
+                }
+            }
+        }
+    }
+    bool empty() {
+        return head == -1;
+    }
+    T top() {
+        return arr[head];
+    }
+    T pop() {
+        count -= 1;
+        return arr[head--];
+    }
+    ~TPQueue() {
+        delete[] arr;
+    }
+};
 
-const T pop() {
-  T res = Query.back();
-  Query.pop_back();
-  return res;
+ostream& operator<<(ostream& os, const SYM& point) {
+    os << point.ch << " " << point.prior << endl;
+    return os;
 }
-};
-
-struct SYM {
-char ch;
-int prior;
-};
