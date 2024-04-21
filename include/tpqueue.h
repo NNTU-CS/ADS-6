182b274479acf29be1c332ef04fe5a70330897d2
maxim.sustaev <maxim.sustaev@yandex.ru>
2024-04-21 21:00:19 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,25 @@
 
 template<typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
+ private:
+    std::list<T> Query;
+ public:
+    void push(T a) {
+        if (!Query.empty()) {
+            for (auto i = Query.cbegin(); i != Query.cend(); ++i) {
+                if (a.prior <= (*i).prior) {
+                    Query.insert(i, a);
+                    return;
+                }
+            }
+        }
+        Query.push_back(a);
+    }
+    const T pop() {
+        T res = Query.back();
+        Query.pop_back();
+        return res;
+    }
 };
 
 struct SYM {
