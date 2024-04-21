7c2bbe1bbed78ed333eea716cdde335ff7b6a25f
AndreyMatuhin99 <tronder.yt@gmail.com>
2024-04-21 13:28:06 +0300

    Задание 6

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,9 +2,20 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
-template<typename T, int size>
+template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
+private:
+    struct ITEM {
+        T data;
+        ITEM* next;
+    };
+    ITEM* head;
+    ITEM* tail;
+public:
+    TPQueue() : head(nullptr), tail(nullptr) {}
+    ~TPQueue();
+    void push(const T& data);
+    T pop();
 };
 
 struct SYM {
@@ -12,4 +23,55 @@ struct SYM {
   int prior;
 };
 
+template<typename T>
+TPQueue<T>::~TPQueue() {
+    while (head)
+    {
+        ITEM* temp = head->next;
+        delete head;
+        head = temp;
+    }
+}
+
+template<typename T>
+void TPQueue<T>::push(const T& data) {
+    ITEM* temp = new ITEM;
+    temp->data = data;
+    temp->next = nullptr;
+
+    if (!head) {
+        head = temp;
+        tail = temp;
+    } else {
+        if (temp->data.prior > head->data.prior) {
+            temp->next = head;
+            head = temp;
+        } else {
+            ITEM* current = head;
+            while (current->next) {
+                if (temp->data.prior > current->next->data.prior)
+                    break;
+                current = current->next;
+            }
+            temp->next = current->next;
+            current->next = temp;
+            if (!temp->next)
+                tail = temp;
+        }
+    }
+}
+
+template<typename T>
+T TPQueue<T>::pop() {
+    if (!head)
+        throw std::out_of_range("Queue is empty");
+
+    ITEM* temp = head;
+    T data = head->data;
+    head = head->next;
+    delete temp;
+    return data;
+}
+
 #endif  // INCLUDE_TPQUEUE_H_
+
