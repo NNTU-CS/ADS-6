43791dcb71dfad7dbf36cd99fa1a4fb329359c32
AndreyMatuhin99 <tronder.yt@gmail.com>
2024-04-21 13:32:32 +0300

    Задание 6

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -18,59 +18,50 @@ class TPQueue {
     ITEM* tail;
  public:
     TPQueue() : head(nullptr), tail(nullptr) {}
-    ~TPQueue();
-    void push(const T& data);
-    T pop();
-};
-
-template<typename T>
-TPQueue<T>::~TPQueue() {
-    while (head)
-    {
-        ITEM* temp = head->next;
-        delete head;
-        head = temp;
-    }
-}
-
-template<typename T>
-void TPQueue<T>::push(const T& data) {
-    ITEM* temp = new ITEM;
-    temp->data = data;
-    temp->next = nullptr;
-
-    if (!head) {
-        head = temp;
-        tail = temp;
-    } else {
-        if (temp->data.prior > head->data.prior) {
-            temp->next = head;
+    ~TPQueue() {
+        while (head)
+        {
+            ITEM* temp = head->next;
+            delete head;
             head = temp;
-        } else {
-            ITEM* current = head;
-            while (current->next) {
-                if (temp->data.prior > current->next->data.prior)
-                    break;
-                current = current->next;
-            }
-            temp->next = current->next;
-            current->next = temp;
-            if (!temp->next)
-                tail = temp;
         }
     }
-}
+    void push(const T& data) {
+        ITEM* temp = new ITEM;
+        temp->data = data;
+        temp->next = nullptr;
 
-template<typename T>
-T TPQueue<T>::pop() {
-    if (!head)
-        throw std::out_of_range("Queue is empty");
+        if (!head) {
+            head = temp;
+            tail = temp;
+        } else {
+            if (temp->data.prior > head->data.prior) {
+                temp->next = head;
+                head = temp;
+            } else {
+                ITEM* current = head;
+                while (current->next) {
+                    if (temp->data.prior > current->next->data.prior)
+                        break;
+                    current = current->next;
+                }
+                temp->next = current->next;
+                current->next = temp;
+                if (!temp->next)
+                    tail = temp;
+            }
+        }
+    }
+    T pop() {
+        if (!head)
+            throw std::out_of_range("Queue is empty");
 
-    ITEM* temp = head;
-    T data = head->data;
-    head = head->next;
-    delete temp;
-    return data;
-}
+        ITEM* temp = head;
+        T data = head->data;
+        head = head->next;
+        delete temp;
+        return data;
+    }
+};
 
 #endif  // INCLUDE_TPQUEUE_H_
