7119a228b6dd0b7c1b0c5878762a42a2abd82fd4
MariaSadkova <160345524+MariaSadkova@users.noreply.github.com>
2024-04-21 15:04:20 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,54 +2,102 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
-template<typename T, const int size>
-class TPQueue {
-  struct Item {
-    T data;
-    int prior;
-  };
-  Item arr[size];
-  int m = 0, end = 0;
+#include <cassert>
+ 
+template<typename T>
+class TPQueue
+{
+private:
+	T* arr;         
+	int size;        
+	int begin,       
+		end;        
+	int count;       
+public:
+	TPQueue(int = 100);          
+	~TPQueue();                 
 
- public:
-  TPQueue() {
-    for (int i = 0; i < size; i++) {
-      arr[i].data = T();
-      arr[i].prior = -10;
-    }
-  }
-  void push(T sym, int priority) {
-    if (end != 0) {
-      int i = end - 1;
-      for (; i >= m; i--) {
-        if (priority > arr[i].prior) {
-          arr[i + 1].data = arr[i].data;
-          arr[i + 1].prior = arr[i].prior;
-        } else {
-          break;
-        }
-      }
-      arr[++i].data = sym;
-      arr[i].prior = priority;
-      end++;
-    } else {
-      arr[0].data = sym;
-      arr[0].prior = priority;
-      end++;
-    }
-  }
-  T pop() {
-    T f = arr[m].data;
-    for (int i = m; i < end - 1; i++) {
-      arr[i].data = arr[i + 1].data;
-      arr[i].prior = arr[i + 1].prior;
-    }
-    end--;
-    return f;
-  }
+	void push(const T&); 
+	T pop();              
+	T get() const;       
+	bool isEmpty() const;      
+	bool isFull() const;       
 };
-struct SYM {
-  char ch;
-  int prior;
+
+template<typename T>
+TPQueue<T>::TPQueue(int sizeQueue) :
+	size(sizeQueue),
+	begin(0), end(0), count(0)
+{
+	arr = new T[size + 1];
+}
+template<typename T>
+TPQueue<T>::~TPQueue()
+{
+	delete[] arr;
+}
+template<typename T>
+void TPQueue<T>::push(const T& item)
+{
+	assert(count < size);
+	if (count == 0)
+	{
+		arr[end++] = item;
+		count++;
+	}
+	else
+	{
+		int i = end-1;
+		bool flag = 0;
+		while (i>=begin && item.prior > arr[i].prior)
+		{
+			flag = 1;
+			arr[i + 1] = arr[i];
+			arr[i] = item;
+			i--;
+		}
+		if (flag == 0)
+			arr[end] = item;
+		end++;
+		count++;
+	}
+
+	if (end > size)
+		end -= size + 1; 
+}
+template<typename T>
+T TPQueue<T>::pop()
+{
+	assert(count > 0);
+
+	T item = arr[begin++];
+	count--;
+
+	if (begin > size)
+		begin -= size + 1;
+
+	return item;
+}
+template<typename T>
+T TPQueue<T>::get() const
+{
+	assert(count > 0);
+	return arr[begin];
+}
+template<typename T>
+bool TPQueue<T>::isEmpty() const
+{
+	return count == 0;
+}
+template<typename T>
+bool TPQueue<T>::isFull() const
+{
+	return count == size;
+}
+
+struct SYM
+{
+	char ch;
+	int  prior;
 };
 #endif  // INCLUDE_TPQUEUE_H_
