90536535cee61c32682c607cd3275daadb641d0f
MariaSadkova <160345524+MariaSadkova@users.noreply.github.com>
2024-04-21 16:39:12 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,6 +1,7 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+#include <string>
 
 template<typename T, int size>
 class TPQueue {
@@ -14,14 +15,14 @@ class TPQueue {
             delete[] arr;
         }
     }
-    void push(const T& b) {
+    void push(const T& bbb) {
         int p = 0;
         while (p < k) {
-            if (arr[p].prior < b.prior) break;
+            if (arr[p].prior < bbb.prior) break;
             p++;
         }
         T t1 = arr[p];
-        arr[p] = b;
+        arr[p] = bbb;
         for (int i = p + 1; i < k + 1; i++) {
             T t2 = arr[i];
             arr[i] = t1;
@@ -31,17 +32,17 @@ class TPQueue {
     }
     T pop() {
         if (!k) throw std::string("Empty");
-        T b = arr[0];
+        T bbb = arr[0];
         k--;
         for (int i = 0; i < k; i++) {
             arr[i] = arr[i + 1];
         }
-        return b;
+        return bbb;
     }
 };
-struct SYM
-{
-	char ch;
-	int  prior;
+struct SYM {
+  char ch;
+  int prior;
 };
+
 #endif  // INCLUDE_TPQUEUE_H_
