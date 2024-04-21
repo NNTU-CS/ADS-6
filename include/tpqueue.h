78d1f1da2d215ea8420ad4c35f63838b0df7d32c
Vita <75259415+Vitacci@users.noreply.github.com>
2024-04-21 12:52:23 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -6,40 +6,40 @@
 
 template<typename T, int size>
 class TPQueue {
-private:
- T* data;
- int begin, end, cnt;
+ private:
+  T* data;
+  int begin, end, cnt;
 
-public:
- TPQueue(): begin(0), end(0), cnt(0), data(new T[size]) {}
- void push(const T& item) {
-   if (cnt >= size) {
+ public:
+  TPQueue(): begin(0), end(0), cnt(0), data(new T[size]) {}
+  void push(const T& item) {
+    if (cnt >= size) {
+      throw std::string("Empty");
+    } else {
+      cnt++;
+      int pos = end;
+      int i = begin;
+      while (i < end) {
+        if (data[i].prior < item.prior) {
+          pos = i;
+          break;
+        }
+        i++;
+      }
+      for (int i = end; i > pos; i--)
+        data[i % size] = data[(i - 1) % size];
+      data[pos % size] = item;
+      end++;
+    }
+  }
+ T pop() {
+   if (cnt == 0) {
      throw std::string("Empty");
    } else {
-     cnt++;
-     int pos = end;
-     int i = begin;
-     while (i < end) {
-       if (data[i].prior < item.prior) {
-         pos = i;
-         break;
-       }
-       i++;
-     }
-     for (int i = end; i > pos; i--)
-       data[i % size] = data[(i - 1) % size];
-     data[pos % size] = item;
-     end++;
+     cnt--;
+     return data[(begin++) % size];
    }
  }
-T pop() {
-  if (cnt == 0) {
-    throw std::string("Empty");
-  } else {
-    cnt--;
-    return data[(begin++) % size];
-  }
-}
 };
 
 struct SYM {
