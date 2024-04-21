05e0ffa69932fdade06bfa7f40fe97c48538c992
MariaSadkova <160345524+MariaSadkova@users.noreply.github.com>
2024-04-21 14:01:59 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,53 @@
 
 template<typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
+private:
+    T* arr; // массив с данными
+    int count; // текущее количество элементов
+    int rear; // индекс конца очереди
+    int front; // индекс начала очереди
+public:
+    TPQueue() : count(0), rear(0), front(0) {
+        arr = new T[size];
+    }
+
+    ~TPQueue() {
+        delete[] arr;
+    }
+
+    void push(const T& item, int prior) {
+        if (count < size) {
+            if (count == 0) {
+                arr[rear] = item;
+            }
+            else {
+                int i = rear - 1;
+                while (i >= front && prior > arr[i].prior) {
+                    arr[(i + 1 + size) % size] = arr[i];
+                    i--;
+                }
+                arr[(i + 1 + size) % size] = item;
+            }
+            rear = (rear + 1) % size;
+            count++;
+        }
+        else {
+            std::cout << "Queue is full" << std::endl;
+        }
+    }
+
+    T pop() {
+        if (count > 0) {
+            T item = arr[front];
+            front = (front + 1) % size;
+            count--;
+            return item;
+        }
+        else {
+            std::cout << "Queue is empty" << std::endl;
+            return T();
+        }
+    }
 };
 
 struct SYM {
