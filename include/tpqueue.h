f8e82e4e03f7c4d13f9fba2288663d1dc2570df7
hellboy2364 <160237839+hellboy2364@users.noreply.github.com>
2024-04-22 13:34:00 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,6 +5,57 @@
 template<typename T, int size>
 class TPQueue {
   // реализация шаблона очереди с приоритетом на кольцевом буфере
+private:
+    SYM* mas;
+    int counter;
+    int head, tail;
+
+public:
+    TPQueue() : counter(0), head(0), tail(0) {
+        mas = new SYM[size + 1];
+        for (int j = 0; j < size; j++)
+            mas[j] = { '-', 0 };
+    }
+    void push_unit(SYM c) {
+        if (!counter) {
+            mas[head] = c;
+            counter++;
+            tail = head;
+        }
+        else {
+            int j = tail;
+            mas[j + 1] = c;
+            while (j >= begin) {
+                if (mas[j].prior < mas[j + 1].prior) {
+                    SYM mp = mas[j];
+                    mas[j] = mas[j + 1];
+                    mas[j + 1] = mp;
+                }
+                j--;
+            }
+            tail = tail + 1;
+            counter++;
+        }
+    }
+
+    SYM pop_unit() {
+        if (!counter) {
+            SYM e{ '0', -2 };
+            return e;
+        }
+        else {
+            SYM mp = mas[head];
+            for (int j = 0; j < counter - 1; j++) {
+                mas[j] = mas[j + 1];
+            }
+            tail--;
+            counter--;
+            return mp;
+        }
+    }
+    ~TPQueue() {
+        delete[]mas;
+    }
 };
 
 struct SYM {
