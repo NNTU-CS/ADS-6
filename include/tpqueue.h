b473ffec18cf3da63e8def208f1592d54e0c66ff
Nikita-Erem <160235350+Nikita-Erem@users.noreply.github.com>
2024-04-22 15:16:35 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,26 @@
 
 template<typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
+private:
+    T data[Size];
+    int begin, end, count;
+
+ public:
+    TPQueue() : begin(0), end(0), count(0) {}
+
+    void push(const T &item) {
+        if (count >= Size) {
+            throw std::string("Full");
+        }
+        int i = end - 1;
+        while (i >= begin && item.prior > data[i % Size].prior) {
+            data[(i + 1) % Size] = data[i % Size];
+            i--;
+        }
+        data[(i + 1) % Size] = item;
+        end = (end + 1) % Size;
+        count++;
+    }
 };
 
 struct SYM {
