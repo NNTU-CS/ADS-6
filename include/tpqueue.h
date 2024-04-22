7b40b33e4f86dadc5ec8aa8467ae03fbfb136055
Nikita-Erem <160235350+Nikita-Erem@users.noreply.github.com>
2024-04-22 15:33:17 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,44 +1,47 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
-#include <stdexcept>
-#include <cstddef>
+#include <string>
 
 template<typename T, int size>
 class TPQueue {
-private:
-    T* data;
-    std::size_t capacity;
-    std::size_t count;
+ private:
+    T data[Size];
+    int a, b, c;
 
  public:
-    TPQueue() : data(new T[Size]), capacity(Size), count(0) {}
+    TPQueue() : a(0), b(0), c(0) {}
 
-    ~TPQueue() {
-        delete[] data;
+    void push(const T &ai) {
+        int p;
+        if (c >= Size) {
+            throw std::string("Full");
+        } else {
+            if (c == 0) {
+                data[a] = ai;
+                c++;
+            } else {
+                for (p = b; p >= a; p--) {
+                    if (ai.prior > data[i % Size].prior) {
+                        data[(i + 1) % Size] = data[i % Size];
+                    } else {
+                        break;
+                    }
+                }
+                b++;
+                data[(i + 1) % Size] = ai;
+                c++;
+            }
+        }
     }
 
-    void push(const T& elem) {
-        if (count == capacity) {
-            throw std::out_of_range("Queue is full");
-        }
-
-        std::size_t i = count;
-        while (i > 0 && data[i - 1].prior >= elem.prior) {
-            data[i] = data[i - 1];
-            --i;
-        }
-        data[i] = elem;
-        ++count;
-    }
     T pop() {
-        if (count == 0) {
-            throw std::out_of_range("Queue is empty");
+        if (c == 0) {
+            throw std::string("Empty");
+        } else {
+            c--;
+            return data[a++ % Size];
         }
-
-    T tmp = arr[count - 1];
-        --count;
-        return tmp;
     }
 };
 
