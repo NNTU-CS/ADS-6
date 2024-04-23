113652a805b1089e54f4e03aa97a3b206d876479
AnatoliyBakshaev <nuklimzy@gmail.com>
2024-04-23 21:48:19 +0300

    ads-6.4

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,7 +1,9 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+
 #include <string>
+
 template<typename T, int Size>
 class TPQueue {
 private:
@@ -33,6 +35,7 @@ public:
             }
         }
     }
+
     T pop() {
         if (k == 0) {
             throw std::string("empty");
@@ -42,6 +45,7 @@ public:
         }
     }
 };
+
 struct SYM {
     char ch;
     int prior;
