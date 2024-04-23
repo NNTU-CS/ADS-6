e506e6eaebef4b5618e2ec2afb89a2694f50b91b
AnatoliyBakshaev <nuklimzy@gmail.com>
2024-04-23 21:53:39 +0300

    ads-6.5

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -6,44 +6,43 @@
 
 template<typename T, int Size>
 class TPQueue {
-private:
-    T inform[Size];
-    int nach, end, k;
+    private:
+        T inform[Size];
+        int nach, end, k;
+        public:
+        TPQueue() : nach(0), end(0), k(0) {}
 
-    public:
-    TPQueue() : nach(0), end(0), k(0) {}
-
-        void push(const T &item) {
-            int i;
-            if (k >= Size) {
-                throw std::string("full");
-            } else {
-                if (k == 0) {
-                    inform[nach] = item;
-                    k++;
+            void push(const T &item) {
+                int i;
+                if (k >= Size) {
+                    throw std::string("full");
                 } else {
-                    for (i = end; i >= nach; i--) {
-                        if (item.prior > inform[i % Size].prior) {
-                            inform[(i + 1) % Size] = inform[i % Size];
-                        } else {
-                            break;
+                    if (k == 0) {
+                        inform[nach] = item;
+                        k++;
+                    } else {
+                        for (i = end; i >= nach; i--) {
+                            if (item.prior > inform[i % Size].prior) {
+                                inform[(i + 1) % Size] = inform[i % Size];
+                            } else {
+                                break;
+                            }
                         }
+                        end++;
+                        inform[(i + 1) % Size] = item;
+                        k++;
                     }
-                    end++;
-                    inform[(i + 1) % Size] = item;
-                    k++;
                 }
             }
-        }
 
-    T pop() {
-        if (k == 0) {
-            throw std::string("empty");
-        } else {
-            k--;
-            return inform[nach++ % Size];
+        T pop() {
+            if (k == 0) {
+                throw std::string("empty");
+            } else {
+                k--;
+                return inform[nach++ % Size];
+            }
         }
-    }
 };
 
 struct SYM {
