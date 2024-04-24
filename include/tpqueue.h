389ab99cfd22bed9bf62720ce73cfcd4f4be6201
Сергей Букарин <SerBuka2006@yandex.ru>
2024-04-24 18:04:31 +0300

    ads 6

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,34 +4,34 @@
 
 template<typename T, int size>
 class TPQueue {
- private:
-  T data[100] = {};
-  int index = 0, start = 0;
+private:
+    T data[100] = {};
+    int index = 0, start = 0;
 
- public:
-  T pop() {
-    start += 1;
-    return data[start - 1];
-  }
-
-  void push(T elem) {
-    data[index] = elem;
-    index += 1;
-    for (int t = start; t < index; t++) {
-      for (int i = start; i < index - 1; i++) {
-        if (data[i + 1].prior > data[i].prior) {
-          T b = data[i];
-          data[i] = data[i + 1];
-          data[i + 1] = b;
-        }
-      }
+public:
+    T pop() {
+        start += 1;
+        return data[start - 1];
     }
-  }
+
+    void push(T elem) {
+        data[index] = elem;
+        index += 1;
+        for (int t = start; t < index; t++) {
+            for (int i = start; i < index - 1; i++) {
+                if (data[i + 1].prior > data[i].prior) {
+                    T b = data[i];
+                    data[i] = data[i + 1];
+                    data[i + 1] = b;
+                }
+            }
+        }
+    }
 };
 
 struct SYM {
-  char ch;
-  int prior;
+    char ch;
+    int prior;
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
