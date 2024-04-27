3a84abc032e88dfe8c09ba0effca070b86610a9f
Dobro <76173176+OlesuaDobro@users.noreply.github.com>
2024-04-27 20:09:23 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -6,10 +6,7 @@
 template<typename T, int size>
 class TPQueue {
  private:
-    T *arr;
-int head;
-int tail;
-int count;
+    T *arr, int head, int tail, int count;
 
  public:
     TPQueue():head(0), tail(0), count(0), arr(new T[size]) {}
