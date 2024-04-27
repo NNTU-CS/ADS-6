4e5ea21b31f528444cbf89cde97b1a9810443169
Dobro <76173176+OlesuaDobro@users.noreply.github.com>
2024-04-27 18:18:03 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,10 +1,11 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+#include <string>
+#include <iostream>
 
 template<typename T, int size>
 class TPQueue {
-
 private:
     T* arr;
     int count;
