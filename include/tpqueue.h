68b8ccbb959895ecfcb7489fa4a13a00efb53c06
DimaArtemev04 <160415382+DimaArtemev04@users.noreply.github.com>
2024-04-29 21:02:08 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,12 +1,30 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+#include <list>
 
 template<typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
+ private:
+    std::list<T> Query;
+ public:
+    void push(T x) {
+        if (!Query.empty()) {
+            for (auto i = Query.cbegin(); i != Query.cend(); i++) {
+                if (x.prior <= (*i).prior) {
+                    Query.insert(i, x);
+                    return;
+                }
+            }
+        }
+        Query.push_back(x);
+    }
+    const T pop() {
+        T res = Query.back();
+        Query.pop_back();
+        return res;
+    }
 };
-
 struct SYM {
   char ch;
   int prior;
