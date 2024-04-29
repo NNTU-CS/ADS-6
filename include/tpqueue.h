34e64f81a81bba1c2efca80493b465f26d6774ac
Trohinic <159806490+Trohinic@users.noreply.github.com>
2024-04-29 13:13:08 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,9 +2,54 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
-template<typename T, int size>
+#pragma once
+#include <iostream>
+
+template <typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
+ private:
+  T arr[size] = {};
+  int start_index = 0;
+  int end_index = 0;
+  int fullness = 0;
+
+  int norm(int i) { return (i + size) % size; }
+
+  void empty_place(int in_index) {
+    for (int i = 0; i < (end_index - in_index + size) % size; ++i) {
+      arr[norm(end_index - i)] = arr[norm(end_index - i - 1)];
+    }
+  }
+
+ public:
+  void push(T new_elem) {
+    if (fullness == size) {
+      return;
+    }
+
+    int in_index = end_index;
+    while (true) {
+      if ((in_index == start_index)
+
+          || ((arr[in_index - 1].prior >= new_elem.prior) &&
+              (arr[in_index - 1].prior != 0))) {
+        break;
+      }
+      in_index = norm(in_index - 1);
+    }
+    empty_place(in_index);
+    fullness += 1;
+    end_index = norm(end_index + 1);
+    arr[in_index] = new_elem;
+  }
+
+  T pop() {
+    T res = arr[start_index];
+    start_index = norm(start_index + 1);
+    fullness -= 1;
+
+    return res;
+  }
 };
 
 struct SYM {
