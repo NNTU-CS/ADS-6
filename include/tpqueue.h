e167af16c93f5876b9112dc0b6dff0946e82fb30
Kotyara21 <73665937+Kotyara21@users.noreply.github.com>
2024-04-30 22:50:47 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,15 +1,51 @@
-// Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
+#include <stdexcept>
+#include <cstddef>
+
 template<typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
+ private:
+    T* buffer;
+    std::size_t capacity;
+    std::size_t count;
+
+ public:
+    TPQueue() : buffer(new T[size]), capacity(size), count(0) {}
+
+    ~TPQueue() {
+        delete[] buffer;
+    }
+
+    void push(const T& item) {
+        if (count == capacity) {
+            throw std::out_of_range("Queue is full");
+        }
+
+        std::size_t index = count;
+        while (index > 0 && buffer[index - 1].priority >= item.priority) {
+            buffer[index] = buffer[index - 1];
+            --index;
+        }
+        buffer[index] = item;
+        ++count;
+    }
+
+    T pop() {
+        if (count == 0) {
+            throw std::out_of_range("Queue is empty");
+        }
+
+        T tmp = buffer[count - 1];
+        --count;
+        return tmp;
+    }
 };
 
-struct SYM {
-  char ch;
-  int prior;
+struct Item {
+  char symbol;
+  int priority;
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
