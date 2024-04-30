4d36e621fb8e509891f0982b9909c58a7c5948bf
Kotyara21 <73665937+Kotyara21@users.noreply.github.com>
2024-04-30 23:40:27 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,10 +1,8 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
-
 #include <stdexcept>
 #include <cstddef>
-
 template<typename Type, int size>
 class TPQueue {
  private:
@@ -20,7 +18,7 @@ class TPQueue {
     }
 
     void push(const Type& element) {
-        if (count == element) {
+        if (count == capacity) {
             throw std::out_of_range("full");
         }
 
