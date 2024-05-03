f4b3f5af3f16feac6770b39f57fe13dc3122effe
xQiwix <159818062+xQiwix@users.noreply.github.com>
2024-05-03 17:45:14 +0300

    Update tpqueueb176.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,38 @@
 
 template<typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
+ private:
+    T *arr;
+    int head, t, cnt;
+
+ public:
+    TPQueue() : head(0), t(0), cnt(0), arr(new T[size]) {}
+    void push(const T& item) {
+        if (cnt >=size) {
+            throw std::string("full");
+        }
+        int index = t;
+        cnt++;
+        for (int i = head; i < t; i++) {
+            if (arr[i].prior < item.prior) {
+                index = i;
+                break;
+            }
+        }
+        for (int i = t; i > index; i--) {
+            arr[i % size] = arr[(i - 1) % size];
+        }
+        arr[index % size] = item;
+        t++;
+    }
+    T pop() {
+        if (cnt == 0) {
+            throw std::string("empty!");
+        } else {
+            cnt--;
+            return arr[head++ % size];
+        }
+    }
 };
 
 struct SYM {
