0d1a27752801c14896fd8938152d6989b735f06d
PivovarovAndrei <160382197+PivovarovAndrei@users.noreply.github.com>
2024-05-04 12:37:19 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,28 +5,28 @@
 template<typename T, int size>
 class TPQueue {
    private:
- T arr[Size];
- int begin, end, count;
+      T arr[Size];
+      int begin, end, count;
 
- public:
-  TPQueue() : begin(0), end(0), count(0) {}
-  ~TPQueue() {}
-  void push(const T& item) {
-    assert(count < Size);
- count++;
-    int i = end;
-    while (i != begin && item.prior > arr[(i - 1 + Size) % Size].prior) {
- arr[i % Size] = arr[(i - 1 + Size) % Size];
- i = (i - 1 + Size) % Size;
-    }
- arr[i % Size] = item;
- end = (end + 1) % Size;
-  }
- T pop() {
-    assert(count > 0);
- T item = arr[begin++ % Size];
- count--;
-    return item;
+    public:
+      TPQueue() : begin(0), end(0), count(0) {}  
+      ~TPQueue() {}
+      void push(const T& item) {
+         assert(count < Size);
+         count++;
+         int i = end;
+         while (i != begin && item.prior > arr[(i - 1 + Size) % Size].prior) {
+            arr[i % Size] = arr[(i - 1 + Size) % Size];
+            i = (i - 1 + Size) % Size;
+         }
+         arr[i % Size] = item;
+         end = (end + 1) % Size;
+      }
+T pop() {
+   assert(count > 0);
+   T item = arr[begin++ % Size];
+   count--;
+   return item;
 };
 
 struct SYM {
