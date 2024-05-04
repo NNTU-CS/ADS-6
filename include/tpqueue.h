45b6e5b677e38aedca9bc60fc5ef8326986ae72d
PivovarovAndrei <160382197+PivovarovAndrei@users.noreply.github.com>
2024-05-04 12:46:43 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,52 +1,33 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+#include <list>
 
-template <typename T>
+template<typename T, int size>
 class TPQueue {
-private:
-    std::vector<T> array;
-
-public:
-    void push(const T& elem) {
-        if (array.empty()) {
-            array.push_back(elem);
-        } else {
-            int i = 0;
-            for (i = 0; i < array.size(); ++i) {
-                if (elem.prior > array[i].prior) {
-                    break;
+  // реализация шаблона очереди с приоритетом на кольцевом буфере
+ private:
+    std::list<T> Query;
+ public:
+    void push(T a) {
+        if (!Query.empty()) {
+            for (auto i = Query.cbegin(); i != Query.cend(); i++) {
+                if (a.prior <= (*i).prior) {
+                    Query.insert(i, a);
+                    return;
                 }
             }
-            array.insert(array.begin() + i, elem);
         }
+        Query.push_back(a);
     }
-
-    void pop() {
-        if (!array.empty()) {
-            array.erase(array.begin());
-        } else {
-            std::cout << "Queue is empty." << std::endl;
-        }
+    const T pop() {
+        T result = Query.back();
+        Query.pop_back();
+        return result;
     }
-
-    T front() const {
-        if (!array.empty()) {
-            return array.front();
-        } else {
-            std::cout << "Queue is empty." << std::endl;
-            return T();
-        }
-    }
-
-    bool isEmpty() const {
-        return array.empty();
-    }
-};
 
 struct SYM {
   char ch;
   int prior;
 };
-
 #endif  // INCLUDE_TPQUEUE_H_
