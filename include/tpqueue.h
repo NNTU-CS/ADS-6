8c67e6a2bf765eff36f908b722227ca86cf928d2
SfDarkFire <160343903+SfDarkFire@users.noreply.github.com>
2024-06-07 20:14:59 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,31 +2,62 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
-template<typename T, int size>
-class TPQueue {
-public:
-    void push(const T& elem) {
-        if (this->empty()) {
-            TQueue<T>::push(elem);
-        } else {
-            bool added = false;
-            for (int i = 0; i < TQueue<T>::size(); ++i) {
-                if (elem.prior > TQueue<T>::operator.prior) {
-                    TQueue<T>::push(i, elem);
-                    added = true;
-                    break;
-                }
-            }
-            if (!added) {
-                TQueue<T>::push(elem);
-            }
-        }
-    }
-};
-
 struct SYM {
   char ch;
   int prior;
 };
 
+template<typename T>
+class TPQueue {
+private:
+  struct Node {
+    T data;
+    Node* next;
+  };
+  Node* first;
+  Node* last;
+public:
+  TPQueue() : first(nullptr), last(nullptr) {}
+  ~TPQueue() {
+    while (first) {
+      Node* tmp = first->next;
+      delete first;
+      first = tmp;
+    }
+  }
+  void push(const T& elem) {
+    Node* temp = new Node{elem, nullptr};
+    if (!first) {
+      first = temp;
+      last = temp;
+    } else {
+      Node* curr = first;
+      Node* prev = nullptr;
+      while (curr && elem.prior <= curr->data.prior) {
+        prev = curr;
+        curr = curr->next;
+      }
+      if (!prev) {
+        temp->next = first;
+        first = temp;
+      } else {
+        prev->next = temp;
+        temp->next = curr;
+        if (!curr) last = temp;
+      }
+    }
+  }
+  T pop() {
+    if (empty())
+      throw “Queue is empty”;
+    T result = first->data;
+    Node* temp = first;
+    first = first->next;
+    delete temp;
+    return result;
+  }
+  bool empty() const {
+    return first == nullptr;
+  }
+};
 #endif  // INCLUDE_TPQUEUE_H_
